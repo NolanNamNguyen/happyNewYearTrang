b1231bc9e6f22bbe4f1164081c68a43f991909bd
NamNguyen <namvannguyen.andy@gmail.com>
2021-04-17 19:00:50 +0700

    help Khoi

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,41 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import RaiseSalary from '../components/RaiseSalary'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import RaiseSalary from "../components/RaiseSalary";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
-  const ohHi = 'Trang do you think you gonna have a really good new year?'
+  const { register, handleSubmit, getValues } = useForm();
+  const ohHi = "";
+  const uploadFile = (data) => {
+    const formData = new FormData();
+    formData.append("file", data.myfile[0]);
+    axios.post("http://10.22.116.208/Generator_T2", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+    console.log('ok lah')
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <RaiseSalary helloMsg={ohHi} isRender={true} ></RaiseSalary>
+      <form onSubmit={handleSubmit(uploadFile)}>
+        <input ref={register} id="KhoiFile" name="myfile" type="file"></input>
+        <button
+          type="submit"
+        >
+          upload
+        </button>
+      </form>
+
+      <RaiseSalary helloMsg={ohHi} isRender={true}></RaiseSalary>
     </div>
-  )
+  );
 }
